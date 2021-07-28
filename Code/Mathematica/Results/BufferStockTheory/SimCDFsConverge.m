34ca7b6d81259323476d28c0f96e580421908454
llorracc <Christopher.Carroll@jhu.edu>
2021-07-28 10:20:02 -0400

    makePublic autoupate

--- Code/Mathematica/Results/BufferStockTheory/SimCDFsConverge.m
+++ Code/Mathematica/Results/BufferStockTheory/SimCDFsConverge.m
@@ -6,9 +6,10 @@ of the CDF of c.
 
 *)
 Print["Running SimCDFsConverge.m"];
+
 If[Not[ModelIsSolved],SolveInfHorizToToleranceAtTarget[ToleranceAtTarget=0.0001]];
 
 NumOfPeopleToSimulate  = 100000;
-NumOfPeriodsToSimulate = 40;
+NumOfPeriodsToSimulate = 80;
 Simulate[NumOfPeopleToSimulate,NumOfPeriodsToSimulate,bStartVal=0.];
 
