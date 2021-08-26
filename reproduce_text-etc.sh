165e2a69d38a95d1b6348800dd9c2fe9e49139d5
llorracc <git@llorracc.org>
2021-08-26 19:40:00 -0400

    makePublic autoupdate

--- reproduce_text-etc.sh
+++ reproduce_text-etc.sh
@@ -14,39 +14,49 @@ scriptDir="$(dirname "$0")"
 # Compile LaTeX files in root directory
 
 for file in BufferStockTheory BufferStockTheory-NoAppendix BufferStockTheory-Slides; do
-    pdflatex -output-directory=LaTeX "$file"
-    pdflatex -output-directory=LaTeX "$file"
+    pdflatex -halt-on-error -output-directory=LaTeX "$file"
+    pdflatex -halt-on-error -output-directory=LaTeX "$file"
     bibtex LaTeX/"$file"
-    pdflatex -output-directory=LaTeX "$file"
-    pdflatex -output-directory=LaTeX "$file"
+    pdflatex -halt-on-error -output-directory=LaTeX "$file"
+    pdflatex -halt-on-error -output-directory=LaTeX "$file"
 done
 
 # Compile All-Figures and All-Tables
 for type in Figures Tables; do
-    pdflatex -output-directory=LaTeX "$type/All-$type"
-    [[ -e LaTeX/"$type/All-$type" ]] && bibtex LaTeX/"$type/All-$type" && pdflatex -output-directory=LaTeX "$type/All-$type"
-    pdflatex -output-directory=LaTeX "$type/All-$type"
+    cmd="pdflatex -halt-on-error -output-directory=LaTeX $type/All-$type"
+    echo "$cmd" ; eval "$cmd"
+    [[ -e LaTeX/"$type/All-$type" ]] && bibtex LaTeX/"$type/All-$type" && pdflatex -halt-on-error -output-directory=LaTeX "$type/All-$type"
+    pdflatex -halt-on-error -output-directory=LaTeX "$type/All-$type"
     mv "LaTeX/All-$type.pdf" "$type"
 done
 
 # All the appendices can be compiled as standalone documents (they are "subfiles")
-# Make a list of all the appendices :
+# Make a list of all the appendices:
 find ./Appendices -name '*.tex' ! -name '*econtexRoot*' ! -name '*econtexPath*' -maxdepth 1 -exec echo {} \; > /tmp/appendices
 
 # For each such file, process it by pdflatex
 # If it contains a standalone bibliography, process that
-# Then rerun pdflatex to complete the processing and move the resulting pdf file
-touch economics.bib ; touch Appendices/economics.bib ; touch LaTeX/economics.bib 
+# Then rerun pdflatex -halt-on-error to complete the processing and move the resulting pdf file
+
+# economics.bib file should be empty (in public repo)
+touch economics.bib ; touch Appendices/economics.bib ; touch LaTeX/economics.bib
+
 while read appendixName; do
     filename=$(basename ${appendixName%.*}) # Strip the path and the ".tex"
-    pdflatex --output-directory=LaTeX "$appendixName"
-    if [[ -e "LaTeX/$filename.aux" ]] ; then
+    cmd="pdflatex -halt-on-error --shell-escape --output-directory=LaTeX $appendixName"
+    cmd="pdflatex -halt-on-error --output-directory=LaTeX $appendixName"
+    echo "$cmd" ; eval "$cmd"
+    if [[ -e "LaTeX/$filename.aux" ]] ; then  # bibfile exists
 	bibtex LaTeX/$filename 
-	pdflatex --output-directory=LaTeX $appendixName
+	pdflatex -halt-on-error --output-directory=LaTeX $appendixName
     fi
-    pdflatex --output-directory=LaTeX "$appendixName"
+    pdflatex -halt-on-error --output-directory=LaTeX "$appendixName"
     mv "LaTeX/$filename.pdf" Appendices
 done < /tmp/appendices
-rm /tmp/appendices economics.bib # Cleanup
-rm BufferStockTheory.pdf
+
+# Cleanup
+rm /tmp/appendices economics.bib 
+[[ -e BufferStockTheory.pdf ]] && rm -f BufferStockTheory.pdf
+
+# Make a copy of the generated PDF 
 cp LaTeX/BufferStockTheory.pdf BufferStockTheory.pdf
