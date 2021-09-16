e59622b8e2df4db3737b075444bf499e7a33d500
gitkraken2748 <gitkraken@llorracc.org>
2021-09-16 09:55:39 -0400

    Modify BST to check requirements - Public update

--- reproduce/computed.sh
+++ reproduce/computed.sh
@@ -8,6 +8,6 @@ pip install -r binder/requirements.txt | tee binder/requirements.out
 
 echo '' ; echo 'Producing figures' ; echo ''
 
-cd "Code/Python"
-ipython BufferStockTheory-Problems-and-Solutions-Source.ipynb
+cd "."
+ipython BufferStockTheory.ipynb
 
