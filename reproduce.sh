ca346fe8191ae28a19590149b3fa8cac85163bea
llorracc <git@llorracc.org>
2021-10-06 15:45:23 -0400

    Remove accidental nested Resources directory - Public update

--- reproduce.sh
+++ reproduce.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
+# Reproduce results then text of the paper 
 scriptDir="$(dirname "$0")"
 
 # Regenerate computed results (figs) needed for compiling paper
 ./reproduce/computed.sh
 
-echo '' ; echo 'Reproduce text of paper' ; echo ''
+echo '' ; echo 'Reproduce text of paper:' ; echo ''
 
+texname=BufferStockTheory
 output_directory='LaTeX'
 
-# Make tikz figures
-cd Figures 
-for f in InequalityPFGICFHWCRIC RelatePFGICFHWCRICPFFVAC Inequalities; do
-    pdflatex --output-format pdf -output-directory="../$output_directory" "$f-tikzMake.tex" >/dev/null
-    mv -f                                          "../$output_directory/$f-tikzMake.pdf" "$f.pdf"
+# Make figures that get made by executing a latex file
+# (they should have a filename ending in Make.tex)
+cd Figures
+# For this paper, only the tikz figures need to be made by pdflatex - others are made by python
+for fName_tikzMake in *Make.tex; do # names of all files ending in Make.tex
+    echo "Processing figure $fName_tikzMake"
+    fName=${fName_tikzMake%_tikzMake.tex} # Remove the '_tikzMake.tex' part of the filename
+    cmd="pdflatex -halt-on-error --output-format pdf -output-directory=../$output_directory $fName_tikzMake"
+    echo "$cmd"
+    eval "$cmd"
+    mv -f                                                             "../$output_directory/$fName" "$fName.pdf"
 done
 cd ..
 
-# Make sure bib resources are available 
-if [[ ! -s "$file.bib" ]]; then  # $file.bib exists and is not empty
-    # economics.bib files should exist if they do not yet 
-    for dir in . Appendices Figures Tables LaTeX Resources/LaTeXInputs; do
-	touch "$dir/economics.bib"
-    done
-fi
-
 # Compile LaTeX files in root directory
-for file in BufferStockTheory BufferStockTheory-NoAppendix BufferStockTheory-Slides; do
+for file in "$texname" "$texname"-NoAppendix "$texname"-Slides; do
     echo '' ; echo "Compiling $file" ; echo ''
-    pdflatex -halt-on-error -output-directory=$output_directory "$file"
-    pdflatex -halt-on-error -output-directory=$output_directory "$file" > /dev/null
+    cmd="pdflatex -halt-on-error -output-directory=$output_directory $file"
+    eval "$cmd"
+    eval "$cmd" # Hide second output to reduce clutter
     bibtex $output_directory/"$file"
-    pdflatex -halt-on-error -output-directory=$output_directory "$file" > /dev/null
-    pdflatex -halt-on-error -output-directory=$output_directory "$file"
+    eval "$cmd" # Hide third output to reduce clutter
+    eval "$cmd" 
     echo '' ; echo "Compiled $file" ; echo ''
 done
 
@@ -45,7 +46,7 @@ for type in Figures Tables; do
 done
 
 # All the appendices can be compiled as standalone documents (they are "subfiles")
-# Make a list of all the appendices:
+# Make a list of all the appendices, put the list in the file /tmp/appendices
 find ./Appendices -name '*.tex' ! -name '*econtexRoot*' ! -name '*econtexPath*' -maxdepth 1 -exec echo {} \; > /tmp/appendices
 
 # For each appendix process it by pdflatex
@@ -54,20 +55,25 @@ find ./Appendices -name '*.tex' ! -name '*econtexRoot*' ! -name '*econtexPath*' 
 
 while read appendixName; do
     filename=$(basename ${appendixName%.*}) # Strip the path and the ".tex"
-    #    cmd="pdflatex -halt-on-error --shell-escape --output-directory=$output_directory $appendixName"
     cmd="pdflatex -halt-on-error                 --output-directory=$output_directory $appendixName"
     echo "$cmd" ; eval "$cmd"
-    if grep -q 'bibliography{' "$appendixName"; then
+    if grep -q 'bibliography{' "$appendixName"; then # it has a bibliography
 	bibtex $output_directory/$filename 
-	eval "$cmd"
+	eval "$cmd" 
     fi
     eval "$cmd"
     mv "$output_directory/$filename.pdf" Appendices
 done < /tmp/appendices
 
-# Cleanup
-rm -f /tmp/appendices economics.bib 
-[[ -e BufferStockTheory.pdf ]] && rm -f BufferStockTheory.pdf
+[[ -e "$texname".pdf ]] && rm -f "$texname".pdf
 
-echo '' ; echo "Paper has been compiled to $output_directory/BufferStockTheory.pdf" ; echo ''
+echo '' 
+
+if [[ -e "$output_directory/$texname.pdf" ]]; then
+    echo "Paper has been compiled to $output_directory/$texname.pdf"
+else
+    echo "Something went wrong and the paper is not in $output_directory/$texname.pdf"
+fi
+
+echo ''
 
